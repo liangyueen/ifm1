81c225b2304d72d38cfe44cdca9cf9c0dcf0e410
liangyen <liangyen@yonyou.com>
2018-09-28 09:54:21 +0800

    去掉 Change-Id: Ica67c0f3341617fe1451ec46d3c7c244950e724f

---
+++ ifm/src/public/nc/vo/ifm/OperatorParam.java
@@ -0,0 +1,180 @@
+/*package nc.vo.ifm;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import nc.vo.obm.pay.PaymentRetMsg;
+
+public class OperatorParam implements Serializable {
+	private static final long serialVersionUID = 3950979857630396988L;
+	*//**
+	 * 主键
+	 *//*
+	private String pk;
+	*//**
+	 * 主键数组
+	 *//*
+	private String[] pks;
+	*//**
+	 * 页面编码 必传
+	 *//*
+	private String pageCode;
+	*//**
+	 * 主键与时间戳的映射关系 必传
+	 *//*
+	private Map<String, String> pkMapTs;
+	*//**
+	 * 主键与单据编号的映射关系 必传
+	 *//*
+	private Map<String, String> pkMapVbillno;
+	*//**
+	 * 主键与行号映射关系
+	 *//*
+	private Map<String, Integer> pkMapRowIndex;
+	*//**
+	 * 表体主键数组(部分承付时使用)
+	 *//*
+	private String[] bodyPKs;
+	*//**
+	 * 是否需要返回数据 必传
+	 *//*
+	private boolean isRet = false;
+	*//**
+	 * 网银处理返回结果(网银补录或者网银支付时用到)
+	 *//*
+	private PaymentRetMsg[] results;
+	*//**
+	 * 拓展属性
+	 *//*
+	private Map<String, String> extParam = new HashMap<>();
+	*//**
+	 * 数据
+	 *//*
+	private String data;
+	*//**
+	 * 按钮编码
+	 * 
+	 * @return
+	 *//*
+	private String actionCode;
+
+	public String getPageCode() {
+		return pageCode;
+	}
+
+	public void setPageCode(String pageCode) {
+		this.pageCode = pageCode;
+	}
+
+	public String[] getPks() {
+		return pks;
+	}
+
+	public void setPks(String[] pks) {
+		this.pks = pks;
+	}
+
+	public Map<String, String> getPkMapTs() {
+		return pkMapTs;
+	}
+
+	public void setPkMapTs(Map<String, String> pkMapTs) {
+		this.pkMapTs = pkMapTs;
+	}
+
+	public boolean isRet() {
+		return isRet;
+	}
+
+	public void setRet(boolean isRet) {
+		this.isRet = isRet;
+	}
+
+	public String getPk() {
+		return pk;
+	}
+
+	public void setPk(String pk) {
+		this.pk = pk;
+	}
+
+	public PaymentRetMsg[] getResults() {
+		return results;
+	}
+
+	public void setResults(PaymentRetMsg[] results) {
+		this.results = results;
+	}
+
+	public String getActionCode() {
+		return actionCode;
+	}
+
+	public void setActionCode(String actionCode) {
+		this.actionCode = actionCode;
+	}
+
+	public String[] getBodyPKs() {
+		return bodyPKs;
+	}
+
+	public void setBodyPKs(String[] bodyPKs) {
+		this.bodyPKs = bodyPKs;
+	}
+
+	public Map<String, String> getPkMapVbillno() {
+		return pkMapVbillno;
+	}
+
+	public void setPkMapVbillno(Map<String, String> pkMapVbillno) {
+		this.pkMapVbillno = pkMapVbillno;
+	}
+
+	public Map<String, Integer> getPkMapRowIndex() {
+		return pkMapRowIndex;
+	}
+
+	public void setPkMapRowIndex(Map<String, Integer> pkMapRowIndex) {
+		this.pkMapRowIndex = pkMapRowIndex;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	*//**
+	 * 获取拓展属性
+	 * 
+	 * @param key
+	 *            属性键
+	 * @return 属性值
+	 *//*
+	public String getExtParam(String key) {
+		if (extParam == null || extParam.isEmpty()
+				|| !extParam.containsKey(key)) {
+			return null;
+		}
+		return extParam.get(key);
+	}
+
+	public Map<String, String> getExtParam() {
+		return extParam;
+	}
+
+	public void setExtParam(Map<String, String> extParam) {
+		this.extParam = extParam;
+	}
+
+	public boolean isBatchOperator() {
+		if (pkMapTs == null || pkMapTs.isEmpty()) {
+			return false;
+		}
+		return pkMapTs.size() > 1 ? true : false;
+	}
+}
+*/
---
+++ ifm/src/public/nc/vo/ifm/OperatorResult.java
@@ -0,0 +1,160 @@
+/*package nc.vo.ifm;
+
+import nccloud.framework.web.ui.pattern.extbillcard.ExtBillCard;
+
+public class OperatorResult {
+	*//**
+	 * 状态标识(0:全部成功，1：全部失败，2：部分成功)
+	 *//*
+	private int status;
+	*//**
+	 * 信息
+	 *//*
+	private String msg;
+	*//**
+	 * 成功个数
+	 *//*
+	private Integer successNum;
+	*//**
+	 * 失败个数
+	 *//*
+	private Integer failNum;
+	*//**
+	 * 总个数
+	 *//*
+	private Integer total;
+	*//**
+	 * 返回数据
+	 *//*
+	private ExtBillCard billCard;
+	*//**
+	 * 返回错误信息
+	 *//*
+	private String[] errormessages;
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	public Integer getSuccessNum() {
+		return successNum;
+	}
+
+	public void setSuccessNum(Integer successNum) {
+		this.successNum = successNum;
+	}
+
+	public Integer getFailNum() {
+		return failNum;
+	}
+
+	public void setFailNum(Integer failNum) {
+		this.failNum = failNum;
+	}
+
+	public Integer getTotal() {
+		return total;
+	}
+
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+
+	public ExtBillCard getBillCard() {
+		return billCard;
+	}
+
+	public void setBillCard(ExtBillCard billCard) {
+		this.billCard = billCard;
+	}
+
+	public String[] getErrormessages() {
+		return errormessages;
+	}
+
+	public void setErrormessages(String[] errormessages) {
+		this.errormessages = errormessages;
+	}
+
+	public OperatorResult(int status, String msg, Integer successNum,
+			Integer total,Integer failNum) {
+		super();
+		this.status = status;
+		this.msg = msg;
+		this.successNum = successNum;
+		this.failNum = failNum;
+		this.total = total;
+	}
+
+	private OperatorResult() {
+		super();
+	}
+
+	*//**
+	 * 构建操作结果
+	 * 
+	 * @param hasSuccFlag
+	 *            是否有成功
+	 * @param hasErrFlag
+	 *            是否有失败
+	 * @param list
+	 *            执行结果汇总列表
+	 * @return
+	 *//*
+	public static OperatorResult buildResult(int totalNum, int sucNmu) {
+		String msg = null;
+		ResultState status = ResultState.FAIL;
+		if (sucNmu > 0 && sucNmu < totalNum) {
+			msg = "部分成功！";
+			status = ResultState.PARTSUCCESS;
+		} else if (sucNmu == totalNum) {
+			msg = "全部成功！";
+			status = ResultState.SUCCESS;
+		} else if (sucNmu==0) {
+			msg = "全部失败！";
+			status = ResultState.FAIL;
+		}
+		return new OperatorResult(status.value, msg,totalNum,sucNmu,totalNum-sucNmu);
+	}
+
+	*//**
+	 * 结果枚举
+	 * 
+	 *//*
+	public static enum ResultState {
+		*//**
+		 * 成功
+		 *//*
+		SUCCESS(0),
+		*//**
+		 * 失败
+		 *//*
+		FAIL(1),
+		*//**
+		 * 部分成功
+		 *//*
+		PARTSUCCESS(2);
+		private int value;
+
+		private ResultState(int value) {
+			this.value = value;
+		}
+
+		public int getValue() {
+			return value;
+		}
+
+	}
+}*/
