33346bdb94ca7edada39c428615dad695e44238d
liglt <liglt@yonyou.com>
2018-10-18 10:39:53 +0800

    投资理财脚本
    Change-Id: I6cb0fdf1e058a847e6d8ecdccade67b3dfae5308
    Signed-off-by: liglt <liglt@yonyou.com>

---
+++ ifm/script/business/pub_ncc_queryscheme/pub_ncc_queryscheme/001.sql
@@ -0,0 +1,9 @@
+insert into pub_ncc_queryscheme(pk_ncc_queryscheme,conditionobj,cuserid,dr,isdefault,isprepared,renderobj,sequenc,ts,areaid,iscomplete,isdelete,ismodified,isquickqs,name,name2,name3,name4,name5,name6,parentid,pk_org) values('1001Z610000000025IXY',null,'~',0,'Y','Y',null,1,'2018-10-15 20:35:50',null,null,'N','N','Y',null,null,null,null,null,null,null,'0001A110000000000EQ8')
+go
+
+insert into pub_ncc_queryscheme(pk_ncc_queryscheme,conditionobj,cuserid,dr,isdefault,isprepared,renderobj,sequenc,ts,areaid,iscomplete,isdelete,ismodified,isquickqs,name,name2,name3,name4,name5,name6,parentid,pk_org) values('1001Z610000000025VC7',null,'~',0,'Y','Y',null,1,'2018-10-15 18:54:49',null,null,'N','N','Y',null,null,null,null,null,null,null,'0001A110000000000EQ8')
+go
+
+insert into pub_ncc_queryscheme(pk_ncc_queryscheme,conditionobj,cuserid,dr,isdefault,isprepared,renderobj,sequenc,ts,areaid,iscomplete,isdelete,ismodified,isquickqs,name,name2,name3,name4,name5,name6,parentid,pk_org) values('1001Z6100000000268D9',null,'~',0,'Y','Y',null,1,'2018-10-15 19:02:11',null,null,'N','N','Y',null,null,null,null,null,null,null,'0001A110000000000EQ8')
+go
+
---
+++ ifm/script/business/pub_workitemconfig/pub_workitemconfig/001.sql
@@ -0,0 +1,9 @@
+insert into pub_workitemconfig(pk_workitemconfig,dr,isdefault,ismp,item,itemindex,itemtype,pk_billtype,resourceid,ts) values('0001Z610000Z4IFM0001',0,'Y','Y','364101',null,null,'0001Z61000000002M568',null,'2018-10-08 10:00:00')
+go
+
+insert into pub_workitemconfig(pk_workitemconfig,dr,isdefault,ismp,item,itemindex,itemtype,pk_billtype,resourceid,ts) values('0001Z610000Z4IFM0002',0,'Y','Y','364201',null,null,'0001Z61000000002M5FZ',null,'2018-10-08 10:00:00')
+go
+
+insert into pub_workitemconfig(pk_workitemconfig,dr,isdefault,ismp,item,itemindex,itemtype,pk_billtype,resourceid,ts) values('0001Z610000Z4IFM0003',0,'Y','Y','364301',null,null,'0001Z61000000002M5PM',null,'2018-10-08 10:00:00')
+go
+
