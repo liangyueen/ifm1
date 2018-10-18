147d73d7527fe5f22300a2c6b52109430c147875
lifft <lifft@yonyou.com>
2018-10-18 16:50:17 +0800

    投资申购  后台提交
    Change-Id: Ia55dd8655f081402e392e2779fb3f10fb84699a2
    Signed-off-by: lifft <lifft@yonyou.com>

--- ifm/src/private/nc/bs/ifm/apply/ace/bp/AceApplyApproveBP.java
+++ ifm/src/private/nc/bs/ifm/apply/ace/bp/AceApplyApproveBP.java
@@ -3,13 +3,10 @@ package nc.bs.ifm.apply.ace.bp;
 import nc.bs.ifm.apply.plugin.bpplugin.ApplyPluginPoint;
 import nc.bs.ifm.apply.rule.TallySendApplyProcessVoucherRule;
 import nc.bs.ifm.pub.rule.RegisterWriteBankAccAfterRule;
-import nc.bs.ifm.redeem.rule.TallySendRedeemProcessVoucherRule;
-import nc.impl.pubapp.pattern.data.bill.BillUpdate;
 import nc.impl.pubapp.pattern.data.bill.template.UpdateBPTemplate;
 import nc.impl.pubapp.pattern.rule.IRule;
 import nc.impl.pubapp.pattern.rule.processer.CompareAroundProcesser;
 import nc.vo.ifm.apply.AggInvestApplyVO;
-import nc.vo.ifm.redeem.AggInvestRedeemVO;
 import nc.vo.pub.VOStatus;
 
 /**
@@ -54,7 +51,6 @@ public class AceApplyApproveBP {
 	}
 
 	private void addAfterRule(AggInvestApplyVO[] vos) {
-
 		IRule<AggInvestApplyVO> rule = null;
 		rule = new TallySendApplyProcessVoucherRule();
 		rule.process(vos);
