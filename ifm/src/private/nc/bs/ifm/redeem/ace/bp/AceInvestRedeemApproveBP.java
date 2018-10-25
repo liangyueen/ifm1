9db1e25e046a5c2112571cdcb50715d33d460f8c
lifft <lifft@yonyou.com>
2018-10-25 20:13:12 +0800

    投资申购   初始化功能修改
    Change-Id: I84e42615624522438db6af7797babc325ffab41c
    Signed-off-by: lifft <lifft@yonyou.com>

--- ifm/src/private/nc/bs/ifm/redeem/ace/bp/AceInvestRedeemApproveBP.java
+++ ifm/src/private/nc/bs/ifm/redeem/ace/bp/AceInvestRedeemApproveBP.java
@@ -1,14 +1,29 @@
 package nc.bs.ifm.redeem.ace.bp;
 
-import nc.bs.ifm.apply.plugin.bpplugin.ApplyPluginPoint;
-import nc.bs.ifm.redeem.rule.InvestRedeemApproveTbbRule;
+import nc.bs.ifm.redeem.plugin.bpplugin.InvestRedeemPluginPoint;
+import nc.bs.ifm.redeem.rule.RegisterWriteBankAccAfterRule;
+import nc.bs.ifm.redeem.rule.TallySendRedeemProcessVoucherRule;
+import nc.bs.ifm.redeem.rule.UpdateIFMApplyRule;
+import nc.bs.pub.contract.rule.UpdateApplyRule;
+import nc.bs.pub.payreceipt.rule.PayReceiptVoucherRule;
+import nc.bs.pubapp.pub.rule.FieldLengthCheckRule;
+import nc.bs.pubapp.pub.rule.FillUpdateDataRule;
+import nc.bs.pubapp.pub.rule.UpdateBillCodeRule;
 import nc.impl.pubapp.pattern.data.bill.BillUpdate;
-import nc.impl.pubapp.pattern.data.bill.template.UpdateBPTemplate;
+import nc.impl.pubapp.pattern.data.bill.template.InsertBPTemplate;
+import nc.impl.pubapp.pattern.rule.ICompareRule;
 import nc.impl.pubapp.pattern.rule.IRule;
+import nc.impl.pubapp.pattern.rule.processer.AroundProcesser;
 import nc.impl.pubapp.pattern.rule.processer.CompareAroundProcesser;
+import nc.vo.cdm.payrcptbankcredit.AggBankPayReceiptVO;
+import nc.vo.ifm.RedeemStatusEnum;
 import nc.vo.ifm.redeem.AggInvestRedeemVO;
+import nc.vo.ifm.redeem.InvestRedeemVO;
+import nc.vo.pub.BusinessException;
 import nc.vo.pub.VOStatus;
-
+import nc.vo.pub.lang.UFDouble;
+import nc.impl.pubapp.pattern.data.bill.template.UpdateBPTemplate;
+import nc.vo.pub.pf.BillStatusEnum;
 /**
  * 标准单据审核的BP
  */
@@ -23,30 +38,70 @@ public class AceInvestRedeemApproveBP {
 	 */
 	public AggInvestRedeemVO[] approve(AggInvestRedeemVO[] clientBills,
 			AggInvestRedeemVO[] originBills) {
+		UpdateBPTemplate<AggInvestRedeemVO> bp = new UpdateBPTemplate<AggInvestRedeemVO>(InvestRedeemPluginPoint.APPROVE);
 		for (AggInvestRedeemVO clientBill : clientBills) {
-			clientBill.getParentVO().setStatus(VOStatus.UPDATED);
+			InvestRedeemVO vo = clientBill.getParentVO();
+			Integer billstatus =   (Integer) RedeemStatusEnum.部分赎回.value();//待审核
+			if(vo.getHoldmoney()!=null){
+				if ((vo.getHoldmoney()).compareTo(UFDouble.ZERO_DBL)<1 ) {
+					billstatus = (Integer) RedeemStatusEnum.全部赎回.value();
+				}
+			}else if(vo.getRedeemnumber()!=null){
+				Integer lastNum = vo.getHoldnumber()-vo.getRedeemnumber();
+				if(lastNum==0){
+					billstatus = (Integer) RedeemStatusEnum.全部赎回.value();
+				}
+			}
+			clientBill.getParentVO().setAttributeValue("billstatus", billstatus);
+			clientBill.getParentVO().setBillstatus(VOStatus.NEW);
+			
+			//若有收益金额，则调用收益接口添加赎回收益
+			if(vo.getRealreaning()!= null){
+				
+			}
 		}
-		BillUpdate<AggInvestRedeemVO> update = new BillUpdate<AggInvestRedeemVO>();
-		AggInvestRedeemVO[] returnVos = update.update(clientBills, originBills);
-		UpdateBPTemplate<AggInvestRedeemVO> bp = new UpdateBPTemplate<AggInvestRedeemVO>(
-				ApplyPluginPoint.APPROVE);
 		this.addBeforeRule(bp.getAroundProcesser());
-		
+//		BillUpdate<AggInvestRedeemVO> update = new BillUpdate<AggInvestRedeemVO>();
+//		AggInvestRedeemVO[] returnVos = update.update(clientBills, originBills);
+		this.addAfterRule(bp.getAroundProcesser());
+		AggInvestRedeemVO[] returnVos =bp.update(clientBills, originBills);
+		this.addAfterRule(clientBills);
 		return returnVos;
+	}
+	
+	
+	
+	private void addAfterRule(
+			CompareAroundProcesser<AggInvestRedeemVO> aroundProcesser) {
+		IRule<AggInvestRedeemVO> rwRule = new RegisterWriteBankAccAfterRule();
+		aroundProcesser.addAfterRule(rwRule);
+		
+	}
+	
+	
+	
+	private void addBeforeRule(
+			CompareAroundProcesser<AggInvestRedeemVO> aroundProcesser) {
+		IRule<AggInvestRedeemVO> checkCCAmountRule = new UpdateIFMApplyRule();
+		aroundProcesser.addBeforeRule(checkCCAmountRule);
+		
 	}
 
 	/**
-	 * 执行前规则
+	 * 修改后规则
 	 * 
-	 * @param aroundProcesser
+	 * @param processor
 	 */
-	private void addBeforeRule(
-			CompareAroundProcesser<AggInvestRedeemVO> aroundProcesser) {
-		/** 进行预算控制，释放预占数,进行预算控制，回写登记单执行数 **/
-		IRule<AggInvestRedeemVO> tbbRule = new InvestRedeemApproveTbbRule();
-		aroundProcesser.addBeforeRule(tbbRule);
-	
-	
+	private void addAfterRule(AggInvestRedeemVO[] vos) {
+		IRule<AggInvestRedeemVO> rule = null;
+		rule = new TallySendRedeemProcessVoucherRule();
+		rule.process(vos);
+		
 	}
-
+	/*private void addAfterRule(CompareAroundProcesser<AggInvestRedeemVO> processer) {
+		
+		IRule<AggInvestRedeemVO> terminateRule = new TallySendRedeemProcessVoucherRule();
+		processer.addAfterRule(terminateRule);
+	}
+*/
 }
